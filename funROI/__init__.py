f147fd2779288bf93da72baffa36fe927b7838c8
Ruimin Gao <rgao76@gatech.edu>
2024-10-06 04:30:18 -0400

    initial commit

---
+++ funROI/__init__.py
@@ -0,0 +1,30 @@
+from .settings import Settings
+
+# Create a settings instance
+_settings = Settings()
+
+def set_bids_deriv_folder(path):
+    _settings.set_bids_deriv_folder(path)
+
+def get_bids_deriv_folder():
+    return _settings.get_bids_deriv_folder()
+
+def set_bids_data_folder(path):
+    _settings.set_bids_data_folder(path)
+
+def get_bids_data_folder():
+    return _settings.get_bids_data_folder()
+
+def set_bids_preprocessed_folder(path):
+    _settings.set_bids_preprocessed_folder(path)
+
+def get_bids_preprocessed_folder():
+    return _settings.get_bids_preprocessed_folder()
+
+def get_bids_preprocessed_folder_relative():
+    return _settings.get_bids_preprocessed_folder_relative()
+
+__all__ = [
+    'first_level',
+    'analysis'
+]
---
+++ funROI/settings.py
@@ -0,0 +1,38 @@
+import os
+
+
+class Settings:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Settings, cls).__new__(cls)
+            cls._instance.bids_data_folder = None
+        return cls._instance
+
+    def set_bids_deriv_folder(self, path):
+        self.bids_deriv_folder = path
+
+    def get_bids_deriv_folder(self):
+        if not self.bids_deriv_folder:
+            raise ValueError("BIDS derivatives folder not set. Please set it using 'set_bids_deriv_folder'.")
+        return self.bids_deriv_folder
+    
+    def set_bids_data_folder(self, path):
+        self.bids_data_folder = path
+
+    def get_bids_data_folder(self):
+        if not self.bids_data_folder:
+            raise ValueError("BIDS data folder not set. Please set it using 'set_bids_data_folder'.")
+        return self.bids_data_folder
+    
+    def set_bids_preprocessed_folder(self, path):
+        self.bids_preprocessed_folder = path
+
+    def get_bids_preprocessed_folder(self):
+        if not self.bids_preprocessed_folder:
+            raise ValueError("BIDS preprocessed folder not set. Please set it using 'set_bids_preprocessed_folder'.")
+        return self.bids_preprocessed_folder
+    
+    def get_bids_preprocessed_folder_relative(self):
+        return os.path.relpath(self.bids_preprocessed_folder, start=self.bids_data_folder)
